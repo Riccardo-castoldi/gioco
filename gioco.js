c309bb709532aee60fbdcfcc8dddd8b52a32d71a
riccardo <castoldi.riccardo@iisgalvanimi.edu.it>
2026-02-25 13:55:08 +0000

    Aggiorna il canvas e implementa la logica di gioco di base

--- gioco.js
+++ gioco.js
@@ -1,6 +1,41 @@
-const canva = document.getElementById("gameCanvas");
-const ctx = canva.getContext("2d");
+var mygamepiece;
 
-ctx.fillStyle = "black";
-ctx.fillRect(30, 30, 100, 20);
-ctx.fillRect(30, 60, 100, 20);
+
+function startGame() {
+    mygamepiece = new component(30, 30, "green", 10, 120);
+    myGameArea.start();
+}
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+
+    this.update = function() {
+        let ctx = myGameArea.context; 
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+}
+
+var myGameArea = {
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+    },
+    clear : function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    } 
+}
+function updateGameArea() {
+    myGameArea.clear(); 
+    mygamepiece.update();
+}
