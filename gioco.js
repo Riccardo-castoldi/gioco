0f3550a9b6a8cc2704dd6b5843752117c3d81d9b
riccardo <castoldi.riccardo@iisgalvanimi.edu.it>
2026-03-27 09:55:37 +0000

    Aggiungi la logica di animazione e gestione degli sprite per il personaggio e manca solo la correzione di update

--- gioco.js
+++ gioco.js
@@ -1,10 +1,48 @@
-var mygamepiece;
+
 
 
 function startGame() {
      myGameArea.start();
-    mygamepiece = new component(30, 30, "green", 10, 120);
+    animatedobject.loadimage(); 
 }
+   
+
+var animatedobject = {
+    speedx:0,
+    speedy:0,
+    width: 60,
+    height: 60,
+    x: 10,
+    y: 120,
+    imagelist: [],
+    contaframe:0,
+    actualframe:0,
+
+    
+  update: function() {
+    this.x += this.speedx;
+    this.y += this.speedy;
+    this.contaframe++;
+    if (this.contaframe == 50) {
+      this.contaframe = 0;
+      this.actualframe = (1 + this.actualframe) % this.imagelist.length;
+      //console.log(this.actualframe);
+      this.image = this.imagelist[this.actualframe];
+    }
+  },
+
+   
+    loadimage: function() {
+        for (imgPath of sprites) {
+      var img = new Image(this.width, this.height);
+      img.src = imgPath;
+      this.imagelist.push(img);
+    }
+    this.image = this.imagelist[this.actualframe];
+  }
+};
+ 
+
 
 function component(width, height, color, x, y) {
     this.width = width;
@@ -25,38 +63,49 @@ function component(width, height, color, x, y) {
 var myGameArea = {
     canvas : document.getElementById("gameCanvas"),
     start : function() {
-        this.canvas.width = 480;
-        this.canvas.height = 270;
+        this.canvas.width = 1000;
+        this.canvas.height = 600;
         this.context = this.canvas.getContext("2d");
        
         this.interval = setInterval(updateGameArea, 20);
     },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    } 
+    },
+    drawGameObject: function(gameObject) {
+    this.context.drawImage(
+      gameObject.image,
+      gameObject.x,
+      gameObject.y,
+      gameObject.width,
+      gameObject.height
+    ); } 
 }
 function updateGameArea() {
     myGameArea.clear(); 
-    mygamepiece.update();
+    animatedobject.update();
+    myGameArea.drawGameObject(animatedobject);
+    myGameArea.drawGameObject(animatedobject);
+
 }
 window.addEventListener('keydown', function(event) {
     if (event.key == "ArrowRight") {
-        mygamepiece.x += 10;
+        animatedobject.speedx = 5;
     }
 });
 window.addEventListener('keydown', function(event) {
     if (event.key == "ArrowLeft") {
-        mygamepiece.x -= 10;
+        animatedobject.speedx = -5;
     }
 });
 window.addEventListener('keydown', function(event) {
     if (event.key == "ArrowUp") {
-        mygamepiece.y -= 10;
+        animatedobject.speedy = -5;
     }
 });
 window.addEventListener('keydown', function(event) {
     if (event.key == "ArrowDown") {
-        mygamepiece.y += 10;
+        animatedobject.speedy = 5;
     }
 });
 
---
+++ sprites.js
@@ -0,0 +1,13 @@
+var sprites = [
+"https://i.ibb.co/fWf1PRg/Run-1.png",
+"https://i.ibb.co/FPKsn8m/Run-2.png",
+"https://i.ibb.co/bj1z0NFJ/Run-3.png",
+"https://i.ibb.co/39kkt7TM/Run-4.png",
+"https://i.ibb.co/TMq3vdQR/Run-5.png",
+"https://i.ibb.co/5Wg7VfKQ/Run-6.png",
+"https://i.ibb.co/hRFbwCPF/Run-7.png",
+"https://i.ibb.co/5XJcXczD/Run-8.png",
+"https://i.ibb.co/twjxtB8B/Run-9.png",
+"https://i.ibb.co/CLX5J85/Run-10.png",
+];
+   
